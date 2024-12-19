516b1e908265e7210ba678161b1c6b1647ae4bc5
mhmdw <mhmdwdyn65@gmail.com>
2024-12-19 19:34:56 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ilsamiApp());
+}
+
+class ilsamiApp extends StatelessWidget {
+  const ilsamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
